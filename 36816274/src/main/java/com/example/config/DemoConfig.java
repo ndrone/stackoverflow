7478352ce68d2048c1f37a052b6e3c8e11a56f02
Nick Drone <nicholas.drone@kroger.com>
2016-04-28 11:14:27 -0400

    Fixed
    Found that the with the ReloadableResourceBundleMessageSource that it isn't restricted to just the classpath. So therefore the entire path must be defined within basename.

--- 36816274/src/main/java/com/example/config/DemoConfig.java
+++ 36816274/src/main/java/com/example/config/DemoConfig.java
@@ -3,7 +3,7 @@ package com.example.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -16,8 +16,8 @@ public class DemoConfig extends WebMvcConfigurerAdapter
     @Bean
     public MessageSource messageSource()
     {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
         messageSource.setFallbackToSystemLocale(false);
         messageSource.setCacheSeconds(0);
         messageSource.setDefaultEncoding("UTF-8");
